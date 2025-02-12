67086d2ec2e060e87192947b8fe291ec02cfdd04
Luvptli <lucxx041@gmail.com>
2025-02-12 13:02:40 +0000

    UI, personaje
    Añadido personaje y movimiento con la camara.

--- NeonHuntStealthAndStrike/Assets/Scripts/UIBehaviour.cs
+++ NeonHuntStealthAndStrike/Assets/Scripts/UIBehaviour.cs
@@ -9,21 +9,57 @@ public class UIBehaviour : MonoBehaviour
     public GameObject canvasMainMenu;
     [SerializeField]
     public GameObject canvasOptions;
+    [SerializeField]
+    public GameObject canvasGame;
+    [SerializeField]
+    public GameObject canvasPause;
 
+    [SerializeField]
+    float valueVolume;
     [SerializeField]
     Slider slideVolume;
     [SerializeField]
+    Image imageMuteO;
+    [SerializeField]
+    Image imageMuteG;
+
+    [SerializeField]
     Toggle toggleCompleteScreen;
+
+    public bool estaJugando;
 
     void Start()
     {
         canvasMainMenu.SetActive(true);
         canvasOptions.SetActive(false);
+        estaJugando = false;
+
+        slideVolume.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume = slideVolume.value;
+        Mute();
+
+        if (Screen.fullScreen)
+        {
+            toggleCompleteScreen.isOn = true;
+        }
+        else
+        {
+            toggleCompleteScreen.isOn = false;
+        }
+    }
+
+    void Update()
+    {
+        slideVolume.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume = slideVolume.value;
+        Mute();
+        PauseGame();
     }
     public void ButtonStart()
     {
         canvasMainMenu.SetActive(false);
         canvasOptions.SetActive(false);
+        estaJugando = true;
     }
 
     public void ButtonOptions()
@@ -34,6 +70,35 @@ public class UIBehaviour : MonoBehaviour
             canvasMainMenu.SetActive(false);
         });
         LeanTween.moveLocalX(canvasOptions, -1920f, 1f);
+        estaJugando = false;
+    }
+
+    public void VolumenSlide(float valor)
+    {
+        estaJugando = false;
+        valueVolume = valor;
+        PlayerPrefs.SetFloat("volumenAudio", valueVolume);
+        AudioListener.volume = valueVolume;
+        Mute();
+    }
+
+    public void Mute()
+    {
+        if (valueVolume == 0)
+        {
+            imageMuteO.enabled = true;
+            imageMuteG.enabled = true;
+        }
+        else
+        {
+            imageMuteO.enabled = false;
+            imageMuteG.enabled = false;
+        }
+    }
+
+    public void ActivarPantallaCompleta(bool toggleCompleteScreen)
+    {
+        Screen.fullScreen = toggleCompleteScreen;
     }
 
     public void ButtonReturn()
@@ -44,11 +109,28 @@ public class UIBehaviour : MonoBehaviour
             canvasOptions.SetActive(false);
         });
         LeanTween.moveLocalX(canvasMainMenu, 0f, 1f);
+        estaJugando = false;
     }
 
     public void ButtonExit()
     {
         Application.Quit();
         Debug.Log("Salir");
+        estaJugando = false;
+    }
+
+    public void PauseGame()
+    {
+        if (estaJugando && Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvasPause.SetActive(true);
+            canvasGame.SetActive(false);
+            estaJugando = false;
+        }
+        else if (estaJugando == false && canvasPause == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvasPause.SetActive(false);
+            canvasGame.SetActive(true);
+        }
     }
 }
