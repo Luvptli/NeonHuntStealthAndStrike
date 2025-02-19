86ff321ecb281cc9d592b950a2d40efcc886e4eb
Luvptli <lucxx041@gmail.com>
2025-02-19 22:46:11 +0000

    Enemigos
    Se pone en pie, falta que me siga los waypoints, pq solo me hace el primero y se queda parado

--- NeonHuntStealthAndStrike/Assets/Scripts/WayPointPatrol.cs
+++ NeonHuntStealthAndStrike/Assets/Scripts/WayPointPatrol.cs
@@ -11,8 +11,8 @@ public class WayPointPatrol : MonoBehaviour
     int m_CurrentWaypointIndex;
     // Start is called before the first frame update
     void Start()
-    {
-        navMeshAgent.SetDestination(waypoints[0].position);   
+    { 
+        navMeshAgent.SetDestination(waypoints[0].position);
     }
 
     // Update is called once per frame
